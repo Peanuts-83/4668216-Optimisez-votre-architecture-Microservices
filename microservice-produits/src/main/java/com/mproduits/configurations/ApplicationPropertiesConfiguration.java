13ce1b91f38313640e260000b5cf84c99e40168b
Peanuts-83 <tranque@free.fr>
2024-06-27 15:57:50 +0200

    configServer OK + refreshAuto > actuator

--- microservice-produits/src/main/java/com/mproduits/configurations/ApplicationPropertiesConfiguration.java
+++ microservice-produits/src/main/java/com/mproduits/configurations/ApplicationPropertiesConfiguration.java
@@ -1,19 +1,18 @@
 package com.mproduits.configurations;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Component;
 
 @Component
 @ConfigurationProperties("product-config")
+@RefreshScope
 public class ApplicationPropertiesConfiguration {
+    private int limitDeProduit;
+    public void setLimitDeProduit(int limitDeProduit) {
+        this.limitDeProduit = limitDeProduit;
+    }
     public int getLimitDeProduit() {
         return limitDeProduit;
     }
-
-    public void setLimitdeProduit(int limitDeProduit) {
-        this.limitDeProduit = limitDeProduit;
-    }
-
-    private int limitDeProduit;
-
 }
